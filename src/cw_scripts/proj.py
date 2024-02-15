e5a38de0c99ab8a82b6bdc84414dbf80983d8378
JuliusJacobitz <julius@jacobitz.de>
2024-02-15 15:50:01 +0100

    rm time stuff

--- src/cw_scripts/proj.py
+++ src/cw_scripts/proj.py
@@ -2,8 +2,7 @@ import time
 
 def project_main():
     # open a text file and write current time to it
-    with open('time.txt', 'w') as f:
-        f.write(str(time.time()))
+    print("hi")
 
 if __name__ == "__main__":
     project_main()
